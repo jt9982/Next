fcc11fcbbe9eba65dade3ef6ef8a5144b9908104
jt9982 <jamest9982@gmail.com>
2017-06-30 03:05:52 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,13 +1,14 @@
 library(shiny)
 if (!exists("all_cities")) all_cities = readRDS("data/cities.rds")
 if (!exists("usa_cities")) usa_cities = readRDS("data/usa_cities.rds")
+if (!exists("Swiss_cities")) Swiss_cities = readRDS("data/Swiss_cities.rds")
 
 shinyUI(fluidPage(
   tags$head(
     tags$link(rel="stylesheet", type="text/css", href="custom_styles.css")
   ),
   
-  title = "Traveling Salesman with Simulated Annealing, Shiny, and R",
+  title = "Traveling Salesman with Simulated Annealing in Swiss Cities, Shiny, and R",
   
   tags$h2(tags$a(href="/traveling-salesman", "Traveling Salesman", target="_blank")),
   
@@ -37,7 +38,7 @@ shinyUI(fluidPage(
   fluidRow(
     column(5,
       h4("Choose a map and which cities to tour"),
-      selectInput("map_name", NA, c("World", "USA"), "World", width="100px"),
+      selectInput("map_name", NA, c("World", "Switzerland", "USA"), "World", width="100px"),
       p("Type below to select individual cities, or", actionButton("set_random_cities", "set randomly", icon=icon("refresh"))),
       selectizeInput("cities", NA, all_cities$full.name, multiple=TRUE, width="100%",
                      options = list(maxItems=30, maxOptions=100, placeholder="Start typing to select some cities...",
